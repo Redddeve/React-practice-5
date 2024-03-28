201a58ffe8f9183fa96a90a643208da0b89e639f
Redddeve <popko.dmitry797@gmail.com>
2024-03-28 12:20:01 +0200

    fix: change router basename

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <BrowserRouter basename="goit-react-hw-05-movies">
+    <BrowserRouter basename="React-practice-5">
       <App />
       <ToastContainer
         position="top-right"
